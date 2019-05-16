1bc29fc49528744f2fd17e83729e546e2563bea9
Boris <boris_bfh@mail.ru>
2019-05-16 23:31:33 +0300

    Full functional is done

--- hr_service/urls.py
+++ hr_service/urls.py
@@ -10,8 +10,9 @@ urlpatterns = [
     path('challenge/', views.EnterChallenge, name='challenge'),
     path(r'<int:id>/challenge/', views.ChallengeView, name='challenge'),
     path(r'<int:id>/challenge/thanks/', views.EndChallenge, name='thanks'),
-    path(r'<int:jedi_id>/<int:planet_id>/candidates/', views.CandidatesView, name='candidates'),
-    path(r'<int:jedi_id>/<int:planet_id>/results/', views.ResultsView, name='results'),
+    path(r'jedi/<int:jedi_id>/planet/<int:planet_id>/candidates/', views.CandidatesView, name='candidates'),
+    path(r'jedi/<int:jedi_id>/planet/<int:planet_id>/<int:candidate_id>/results/', views.ResultsView, name='results'),
+    path(r'jedi/<int:jedi_id>/planet/<int:planet_id>/<int:candidate_id>/results/padawan/', views.BecomePadawan, name='padawan'),
 
     #path('create/', views.Create, name='create'),
     #path('<int:candidate_id>/candidate/', views.CandidatesView, name='candidates'),
--- hr_service/views.py
+++ hr_service/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views import generic
 from django.urls import reverse
-from .models import Candidate, Planet, Jedi, Challenge, Answer
+from .models import Candidate, Planet, Jedi, Challenge, Answer, Padawan
+from django.core.mail import send_mail
 
 # Create your views here.
 
@@ -12,14 +13,18 @@ def Index(request):
 
 def JediView(request):
     template = 'hr_service/jedi.html'
-
     jedis = Jedi.objects.all()
-    return render(request, template, {'jedis': jedis})
+    
+    #добавляем поле "кол-во падаванов"
+    jedis_and_padawans = {}
+    for jedi in jedis:
+        jedis_and_padawans[jedi] = Padawan.objects.filter(jedi_id=jedi.id).count()
+    
+    return render(request, template, {'jedis_and_padawans': jedis_and_padawans})
 
 def CandidateView(request):
     template = 'hr_service/candidate.html'
     planets = Planet.objects.all()
-
     return render(request, template, {'planets': planets})
 
 def EnterChallenge(request):
@@ -30,10 +35,10 @@ def EnterChallenge(request):
         age = request.POST['age']
         email = request.POST['email']
 
+        #добавляем нового кандидата в БД
         new_candidate = Candidate.objects.create(name=name, planet=planet, age=age, email=email)
     except:
         return render(request, template)
-        #return HttpResponse(request, 'hr_service/candidate.html')
     else:
         return HttpResponseRedirect(reverse('candidates:challenge', args=(new_candidate.id,)))
 
@@ -46,11 +51,10 @@ def ChallengeView(request, id):
 
 def EndChallenge(request, id):
     template = 'hr_service/thanks.html'
-
     results = request.POST.getlist('answer')
     candidate_name = Candidate.objects.get(pk=id).name
 
-    answers = []
+    answers = [] #получаем все ответы кандидата и сохряняем 
     for i in Challenge.objects.all():
         if str(i.id) in results:
             answers.append(Answer(result=True, candidate_id=id, question_id=i.id))
@@ -62,13 +66,37 @@ def EndChallenge(request, id):
 
 def CandidatesView(request, planet_id, jedi_id):
     template = 'hr_service/candidates.html'
-    candidates = Candidate.objects.filter(planet_id=planet_id)
+    #candidates = Candidate.objects.filter(padawan__candidate_id=1, planet_id=planet_id)
+    
+    candidates = [] #фильтрация по планете и проверка является ли кандидат уже падаваном
+    for candidate in Candidate.objects.filter(planet_id=planet_id):
+        if Padawan.objects.filter(candidate_id=candidate.id).count() is 0:
+            candidates.append(candidate)
 
-    #return HttpResponseRedirect(reverse('candidates:results', args=(planet_id, jedi_id, )))
-    return render(request, template, {'candidates':candidates})
+    return render(request, template, {'candidates':candidates, 'jedi_id':jedi_id})
 
-def ResultsView(request, planet_id, jedi_id):
+def ResultsView(request, planet_id, jedi_id, candidate_id):
     template = 'hr_service/candidate_result.html'
+    candidate_name = Candidate.objects.get(pk=candidate_id).name
+    answers = Answer.objects.filter(candidate_id=candidate_id)
+    
+    #создаем словарь "вопрос":"ответ"
+    result_data = {}
+    for answer in answers:
+        result_data[Challenge.objects.get(pk=answer.question_id).question] = answer.result
 
-    #return HttpResponseRedirect(reverse('candidates:challenge', args=(planet_id, jedi_id, )))
-    return render(request, template)
+    return render(request, template, {'candidate_name':candidate_name, 'result_data':result_data})
+
+def BecomePadawan(request, planet_id, jedi_id, candidate_id):
+    template = 'hr_service/padawan.html'
+
+    #ограничение на кол-во падаванов
+    if Padawan.objects.filter(jedi_id=jedi_id).count() > 3:
+        return HttpResponseRedirect(reverse('candidates:candidates', args=(planet_id, jedi_id, )))
+    
+    Padawan.objects.create(candidate_id=candidate_id, jedi_id=jedi_id)
+    candidate = Candidate.objects.get(pk=candidate_id)
+
+    send_mail('Поздравляем !', 'Поздравляем, вы зачислены в Падаваны !', 'hr@jediacademy.com',[candidate.email])
+
+    return render(request, template, {'candidate':candidate})
